99b9cb75934b8fdb6d837c8f71e17655b105c709
felix-wang-0307 <hwang779@usc.edu>
2024-12-08 13:52:42 -0800

    Initial Commit

---
+++ WeatherSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherSearch
+//
+//  Created by Waterdog on 2024/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherSearch/WeatherSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSearchApp.swift
+//  WeatherSearch
+//
+//  Created by Waterdog on 2024/12/8.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
