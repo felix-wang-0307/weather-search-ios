2dffd2602c3e90d7ff0c35b8b0e23ff87326fffc
felix-wang-0307 <hwang779@usc.edu>
2024-12-09 05:34:47 -0800

    Initial Commit

---
+++ WeatherSearchIOS/WeatherSearchIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSearchIOSApp.swift
+//  WeatherSearchIOS
+//
+//  Created by Waterdog on 2024/12/9.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSearchIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
